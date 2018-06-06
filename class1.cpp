24c78fba3876cd1e79f72428182047d029719abc
Christian <ccastro002@citymail.cuny.edu>
2018-06-06 00:03:45 -0400

    This was the first and second class of csc212

---
+++ class1.cpp
@@ -0,0 +1,91 @@
+
+int main (){
+
+	return 0;
+/*NOTE FIRST CLASS 
+ *  * What is a variable ?
+ *  2. How many primative types?
+ *  3. WHat is a statement ?
+ *  4. What is an expression ?
+ *  5. What is a selection statemtent ?
+ *  6. WHat is a switch block ?
+ *
+ *  In this class 50 to 60 % it will be about coding the rest will include data structure.
+ *  Practice will be based on assignments - take notes,ask questions 
+ *  We will be using piazza to ask questions, emial and office hours. 
+ *  In blackboard theres self practice assignments - no points just for practice 
+ *  For assignments theres a bar to reach to obtain point for it, if the bar is not reached then 
+ *  you will not receive credit.
+ *
+ *  TIME AND EFFORT - I NEEED AN A 
+ *  Before all classes review class notes. 
+ *  Office hours 8-208 after class. 
+  
+*/
+/*NOTE SECOND CLASS
+ * A.Topics
+ * Data structure - specification,design, implementation and use of basic data types
+ *    1. arrays
+ *    2. list 
+ *    3. queues
+ *    4.tress ... 
+ * OOP AND C++ - clases, container, classes ,big tree
+ * Standard templete libary - templetes, iterators
+ *
+ * Data structure is the organization of data in memory. Put boundaries on memory to 
+ * seperate part of memory to have faster acesss of memory.
+ * Store data in a specific place and later retrieve that data.
+ * Goal : Have fast access to the data and store as less as possible.
+ * In this cource we assume that there is a computer with unlimited random access storage and 
+ * processing power. 
+ *     - random acess means having acess to to any location of the storage at any time.
+ * Using this model we compare the performnace of different data structures 
+ *     -during this course we assume our computing model has a single computational node
+ * Data structures are not compared in different computers, we do it scientfically. 
+ * You can't compare data structures with time
+ *     - we do it with the number of steps of the data structure. 
+ *     - size of input is very important
+ *     -e.g if you have n  elements unsorted the its averag3 : n/2
+ *     - if the list is already sorted - average: is log (n )
+ *
+ * B. What is data structures ?
+ *  -data sturcture is the organization of data in the storage and the acess method to the data
+ *  -organization of data is how physical data is stored. It is not related to the hardware storage
+ *  (Data structures might be designed for a specific type of hardware storage though)
+ *  -acess method means how the stored data will be accessed (inset, read , update, delte,..) 
+ *  the running time prefor these access methods is very important. 
+ *  e.g of data structure : arrays, linked list, tress.
+ * 
+ * Real world examples
+ * 1. a building can be seen as a structure, it has multiple floors and each floor may have multiple units, each 
+ * unit also may have multiple rooms.
+ * 2.To have access to a specific unit you have to know the floor number of the unit. Then have access to the floor
+ * using an elevator and have access ot the unit by knowing the number of the unit.
+ * 3. data structures is similar, e.g if you know that you have stored a specific number in the 10th location of a linked
+ * list then you need to have access to that location using a specific algorithm that runs on each link until it reaches  
+ * the specific location. 
+ *
+ * C. What is algorithm?
+ * data structure can't be define without algorihtm.
+ * 1. its a step by step procedure with a specific goal
+ * 2. the input of the an algorith is called the input instance and is an input of the correct algorithm algorithm halts on 
+ * the correct output.
+ * 3. During this course I dont talk about parallel algorithms so the default assumption is our algorithms 
+ * run on a single processor. 
+ * 4. A randomized algorith is a special type of algorithm that has random steps this either the running time, or the output 
+ *  are random.
+ *
+ *  https://www.cs.cmu.edu/~adamchik/15-121/lectures/Sorting%20Algorithms/sorting.html    // some sorting algorithms. 
+ *  
+ * D . What do you need to know before designing a data structure? 
+ *  - what is the max number of elements 
+ *  - what is the max size of the data
+ *  - what is the infastructure that the data structure is going to be used on (e.g #of processors,type,speed,memory size,..)
+ *  - how often the data structure is going to be accessed and what is the type of access
+ *  - is there any power constraints. 
+
+al
+
+
+
+
